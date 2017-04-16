f8b7c678c1330ee01d2a73a81af1799e2fca0bc1
BenYahiaMaryem <ben.yahia.maryem@gmail.com>
2017-04-16 12:04:17 +0200

    advanced research completed just miss prices

--- src/app/store/rapid-search/rapid-search.component.ts
+++ src/app/store/rapid-search/rapid-search.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import {BooksService} from '../../services/books.service';
 @Component({
   selector: 'app-rapid-search',
   templateUrl: './rapid-search.component.html',
@@ -7,9 +7,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RapidSearchComponent implements OnInit {
 
-  constructor() { }
+books: any[];
+
+book:any = {
+    
+    attr:'RapidSearch' 
+}
+
+  constructor(private bookService:BooksService) { }
 
   ngOnInit() {
+    this.book = {
+        
+      attr:''        
+    }
   }
+
+   public save(isValid: boolean, f: any,book:any) {
+        if(f)
+        {
+          console.log(book)
+         this.bookService.getBookAdvancedSearch(book).subscribe( books => {
+            this.books = books;
+             console.log(books)
+          })
+        }
+    }
+
 
 }
